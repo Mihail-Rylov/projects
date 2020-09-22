042dfd1fad516b541f227cbc044596ae05dd7e0a
Mihail-Rylov <Mihail-Rylov@users.noreply.github.com>
2020-09-22 18:57:29 +0300

    My projects

---
+++ Исследование задач поиска по дереву/WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "сформировать дерево";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 100);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(514, 186);
+            this.listBox1.TabIndex = 1;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Enabled = false;
+            this.checkBox1.Location = new System.Drawing.Point(114, 292);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(74, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "подробно";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "распечатка дерева:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(176, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "введите элементы через пробел:";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.HorizontalExtent = 7000;
+            this.listBox2.HorizontalScrollbar = true;
+            this.listBox2.Location = new System.Drawing.Point(15, 320);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.ScrollAlwaysVisible = true;
+            this.listBox2.Size = new System.Drawing.Size(581, 147);
+            this.listBox2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 289);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "обход дерева:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(384, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "заполнить дерево случайно";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(384, 71);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(381, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(170, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "введите количество элементов:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(225, 55);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(222, 78);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 12;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(225, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "найти";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(222, 39);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(148, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "введите искомое значение:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(187, 78);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 478);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Исследование задач поиска по дереву/WindowsFormsApp5/Form1.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int k = 0;
+        private int iskznach;
+        private string str1 = null;
+        private string str2 = null;
+        private string str3 = null;
+        private string str4 = null;
+        private string s1 = null;
+        private string s2 = null;
+        private string s3 = null;
+        private string s4 = null;
+        private string o1 = "В глубину CLR:";
+        private string o2 = "В глубину LCR:";
+        private string o3 = "В глубину RCL:";
+        private string o4 = "В ширину:";
+        private BinaryTree Tree = new BinaryTree();
+        private Random rand = new Random();
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            checkBox1.Enabled = true;
+            string[] words = textBox1.Text.Split(new char[] { ' ' });
+            for (int i = 0; i < words.Length; i++)
+                Tree.Add(Convert.ToInt32(words[i]));
+            Tree.PrintTree(listBox1);
+            ObhodTree();
+            Kratko();
+            button1.Enabled = false;
+            button2.Enabled = false;
+            
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            if (checkBox1.Checked == true)
+            {
+                Podrobno();
+            }
+            else
+            {
+                Kratko();
+            }  
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            checkBox1.Enabled = true;
+            int n = Convert.ToInt32(textBox2.Text);
+            for (int i = 0; i < n; i++)
+                Tree.Add(rand.Next(0, 21));
+            Tree.PrintTree(listBox1);
+            ObhodTree();
+            Kratko();
+            button1.Enabled = false;
+            button2.Enabled = false;
+            
+        }
+        private void Kratko()
+        {
+            listBox2.Items.Add(o1);
+            listBox2.Items.Add(s1);
+            listBox2.Items.Add(o2);
+            listBox2.Items.Add(s2);
+            listBox2.Items.Add(o3);
+            listBox2.Items.Add(s3);
+            listBox2.Items.Add(o4);
+            listBox2.Items.Add(s4);
+        }
+        private void Podrobno()
+        {
+            listBox2.Items.Add(o1);
+            listBox2.Items.Add(str1);
+            listBox2.Items.Add(o2);
+            listBox2.Items.Add(str2);
+            listBox2.Items.Add(o3);
+            listBox2.Items.Add(str3);
+            listBox2.Items.Add(o4);
+            listBox2.Items.Add(str4);
+        }
+        private void ObhodTree()
+        {
+            Tree.CLR(Tree.root, ref str1, ref s1, ref iskznach, ref k);
+            k = 0;
+            Tree.LCR(Tree.root, ref str2, ref s2);
+            Tree.RCL(Tree.root, ref str3, ref s3);
+            Tree.Across(Tree.root, ref str4, ref s4);
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //if (Tree.root.data == iskznach) k--;
+            iskznach = Convert.ToInt32(textBox3.Text);
+            k = 0;
+            Tree.CLR(Tree.root, ref str1, ref s1, ref iskznach, ref k);
+            label5.Text = k.ToString();
+            if (k != 0)
+                label7.Text = "искомых значений найдено: " + k;
+            else
+                label7.Text = "искомое значение не найдено";
+        }
+    }
+    class TreeElement
+    {
+        public int data;
+        public TreeElement left, right;
+
+        public TreeElement(int d)
+        {
+            data = d;
+        }
+    }
+    class BinaryTree
+    {
+        public TreeElement root;
+        public int Length = 0;
+        protected int lvl;
+
+        private void Print(TreeElement link, ListBox list)
+        {
+            if (link.left != null)
+            {
+                lvl++;
+                list.Items.Add('|' + new string('|', lvl) + link.left.data.ToString());
+                Print(link.left,list);
+            }
+            if (link.right != null)
+            {
+                lvl++;
+                list.Items.Add('|' + new string('|', lvl) + link.right.data.ToString());
+                Print(link.right,list);
+            }
+            lvl--;
+        }
+
+        public void PrintTree(ListBox list)
+        {
+            if (root != null)
+            {
+               list.Items.Add('|' + root.data.ToString());
+                Print(root,list);
+            }
+            else
+                list.Items.Add("Дерево пусто");
+            lvl = 0;
+        }
+
+        private void Add_rec(int d, TreeElement link)
+        {
+            if (d < link.data)
+                if (link.left == null)
+                {
+                    link.left = new TreeElement(d);
+                    Length++;
+                }
+                else
+                    Add_rec(d, link.left);
+            if (d >= link.data)
+                if (link.right == null)
+                {
+                    link.right = new TreeElement(d);
+                    Length++;
+                }
+                else
+                    Add_rec(d, link.right);
+        }
+
+        public void Add(int d)
+        {
+            if (root == null)
+            {
+                root = new TreeElement(d);
+                Length++;
+            }
+            else
+                Add_rec(d, root);
+        }
+
+        // прямой обход (CLR - center, left, right)
+        public void CLR(TreeElement node, ref string str, ref string s, ref int iskznach,ref int k)
+        {
+            /*
+             Аргументы метода:
+             1. TreeNode node - текущий "элемент дерева" (ref  передача по ссылке)       
+             2. ref string s - строка, в которой накапливается результат (ref - передача по ссылке)
+            */
+            
+            if (node != null)
+            {
+
+                str += "    получили значение " + node.data.ToString() + Environment.NewLine;
+                if (node.data == iskznach) k++;
+                int temp = node.data;
+                s += node.data.ToString() + " ";
+                str += "    обходим левое поддерево" + Environment.NewLine;
+                CLR(node.left, ref str, ref s, ref iskznach, ref k); // обойти левое поддерево
+                //if (temp == iskznach) k--;
+                str += "    обходим правое поддерево" + Environment.NewLine;
+                CLR(node.right, ref str, ref s, ref iskznach, ref k); // обойти правое поддерево
+            }
+            else str += "    значение отсутствует - null" + Environment.NewLine;
+        }
+        // централизованный обход (LCR - left, center, right) 
+        public void LCR(TreeElement node, ref string str, ref string s)
+        {
+            /*
+             Аргументы метода:
+             1. TreeNode node - текущий "элемент дерева" (ref - передача по ссылке)       
+             2. ref string s - строка, в которой накапливается результат (ref - передача по ссылке)
+            */
+            if (node != null)
+            {
+                str += "    обходим левое поддерево" + Environment.NewLine;
+                LCR(node.left, ref str, ref s); // обойти левое поддерево
+                str += "    получили значение " + node.data.ToString() + Environment.NewLine;
+                s += node.data.ToString() + " ";
+                str += "    обходим правое поддерево" + Environment.NewLine;
+                LCR(node.right, ref str, ref s); // обойти правое поддерево
+            }
+            else str += "    значение отсутствует - null" + Environment.NewLine;
+        }
+        // обратный обход (RCL -right, center, left)
+        public void RCL(TreeElement node, ref string str, ref string s)
+        {
+            /*
+             Аргументы метода:
+             1. TreeNode node - текущий "элемент дерева" (ref  передача по ссылке)       
+             2. ref string s - строка, в которой накапливается результат (ref - передача по ссылке)
+            */
+            if (node != null)
+            {
+                str += "    обходим правое поддерево" + Environment.NewLine;
+                RCL(node.right, ref str, ref s); // обойти правое поддерево
+
+                str += "    получили значение " + node.data.ToString() + Environment.NewLine;
+                s += node.data.ToString() + " ";
+                str += "    обходим левое поддерево" + Environment.NewLine;
+                RCL(node.left, ref str, ref s); // обойти левое поддерево
+            }
+            else  str += "    значение отсутствует - null" + Environment.NewLine;
+        }
+        // обход дерева в ширину (итерационно, используется очередь)
+        public void Across(TreeElement node, ref string str, ref string s)
+        {
+            /*
+             Аргументы метода:
+             1. TreeNode node - текущий "элемент дерева" (ref  передача по ссылке)       
+             2. ref string s - строка, в которой накапливается результат (ref - передача по ссылке)
+            */
+            var queue = new Queue<TreeElement>(); // создать новую очередь
+            str += "    заносим в очередь значение " + node.data.ToString() + Environment.NewLine;
+            s += node.data.ToString() + " ";
+            queue.Enqueue(node); // поместить в очередь первый уровень
+            while (queue.Count != 0) // пока очередь не пуста
+            {
+                //если у текущей ветви есть листья, их тоже добавить в очередь
+                if (queue.Peek().left != null)
+                {
+                    str += "    заносим в очередь значение " + queue.Peek().left.data.ToString() + " из левого поддерева" + Environment.NewLine;
+                    s += queue.Peek().left.data.ToString() + " ";
+                    queue.Enqueue(queue.Peek().left);
+                }
+                if (queue.Peek().right != null)
+                {
+                    str += "    заносим в очередь значение " + queue.Peek().right.data.ToString() + " из правого поддерева" + Environment.NewLine;
+                    s += queue.Peek().right.data.ToString() + " ";
+                    queue.Enqueue(queue.Peek().right);
+                }
+                //извлечь из очереди информационное поле последнего элемента
+                str += "    извлекаем значение из очереди: " + queue.Peek().data.ToString() + Environment.NewLine;
+                queue.Dequeue();
+            }
+        }
+    }
+}
